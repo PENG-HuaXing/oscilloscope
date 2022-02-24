8199d7a74ba9a5f17ad5ced4857d0d0527e500f3
penghuaxing <penghuaxing@ihep.ac.cn>
2022-02-24 11:44:22 +0800

    加入了关于scale变量的设置

--- PmtSinglePhotonSpectrum.py
+++ PmtSinglePhotonSpectrum.py
@@ -4,7 +4,7 @@ from PmtDataSetTool import DataSetTool
 
 
 class SinglePhotonSpectrum(object):
-    def __init__(self, pd_data: pd.DataFrame):
+    def __init__(self, pd_data: pd.DataFrame, scale: float = 1):
         """
         此类为SPE类，功能比较简单，主要有两种方式读取SPE数据：
         1. 使用pandas初始化
@@ -15,6 +15,7 @@ class SinglePhotonSpectrum(object):
         self.max = self.pd_data["Q"].max()
         self.min = self.pd_data["Q"].min()
         self.num = len(pd_data)
+        self.scale = scale
 
     @classmethod
     def load_csv(cls, file: str):
@@ -24,20 +25,41 @@ class SinglePhotonSpectrum(object):
         else:
             print("File is wrong!")
 
-    def get_info(self):
-        return {"min": self.min, "max": self.max, "num": self.num}
+    def get_info(self, scale: float = 1):
+        if scale == 1:
+            return {"min": self.min, "max": self.max, "num": self.num}
+        else:
+            scale_q = self.get_charge(scale)
+            return {"min": scale_q.min(), "max": scale_q.max(), "num": self.num}
 
     def get_pandas_data(self) -> pd.DataFrame:
         return self.pd_data
 
-    def get_charge(self) -> np.ndarray:
-        return self.pd_data["Q"].to_numpy()
+    def get_charge(self, scale: float = 1) -> np.ndarray:
+        return scale * self.pd_data["Q"].to_numpy()
 
-    def proportion(self, division: float) -> tuple[pd.DataFrame, pd.DataFrame]:
+    def get_scale(self):
+        return self.scale
+
+    def proportion(self, division: float, scale: float = 1) -> tuple[pd.DataFrame, pd.DataFrame]:
         """
         返回一个组元，其中分别为低于指定电荷量的数据
         和高于指定电荷量的数据
         """
-        part1 = self.pd_data[self.pd_data["Q"] < division]
-        part2 = self.pd_data[self.pd_data["Q"] > division]
-        return part1, part2
+        if scale == 1:
+            part1 = self.pd_data[self.pd_data["Q"] < division]
+            part2 = self.pd_data[self.pd_data["Q"] > division]
+            return part1, part2
+        else:
+            scale_pd = scale * self.pd_data["Q"]
+            part1 = scale_pd[scale_pd < division]
+            part2 = scale_pd[scale_pd > division]
+            return part1, part2
+
+
+if __name__ == "__main__":
+    from PmtDataSetTool import DataSetTool
+    spe = SinglePhotonSpectrum(DataSetTool.read_file("./source/1353V.csv"))
+    print(spe.get_info(-1e11)["min"])
+    p1, p2 = spe.proportion(2, -1e11)
+    print(p1, p2)
